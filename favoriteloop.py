939e83024a428cdc2edbeed77255c5eaab04d87c
Makeshift123 <marlinmitha69@gmail.com>
2020-01-27 18:18:57 +0200

    dictionary loop tests

---
+++ favoriteloop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.7
+
+favorite_languages = {'jen': 'python', 'sarah': 'c', 'edward': 'ruby', 'phil': 'python'}
+
+for name in sorted(favorite_languages.keys()):
+    print(f"\n{name.try:
+        pass
+    except expression as identifier:
+        pass}")
